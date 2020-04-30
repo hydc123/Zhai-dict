b02ae05a57e2128d718a9fe193627c250e392eca
lunacia <xpwanghy@hotmail.com>
2020-04-30 18:33:37 +0800

    初始化项目

---
+++ neet-words/src/main/java/org/cloudplayer/neetwords/controller/LoginController.java
@@ -0,0 +1,16 @@
+package org.cloudplayer.neetwords.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	private static final String APP_ID = "aaa";
+	private static final String APP_SECRET = "bbb";
+
+
+	@PostMapping("/login")
+	public
+}
